b2eb90f4c2409830d972639963eec0809250aaf6
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-22 16:31:24 -0300

    207. GetHashCode e Equals, implementacao da classe com o override

---
+++ GetHashCodeeEquals/Entities/Clients.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace GetHashCodeeEquals.Entities
+{
+    public class Clients
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(! (obj is Clients) )
+            {
+                return false;
+
+            }
+
+            Clients other = obj as Clients;
+
+            return Email.Equals(other.Email);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+
+        }
+    }
+}
--- GetHashCodeeEquals/Program.cs
+++ GetHashCodeeEquals/Program.cs
@@ -1,43 +1,55 @@
 ﻿using System;
 
+using GetHashCodeeEquals.Entities;
+
 namespace GetHashCodeeEquals
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string a = "Maria";
-            string b = "Alex";
+            //Como o GetHashCode é mais rápido
+            //Então quando eu achar um elemento que tem o mesmo HASHCODE...
+            //Daí sim a gente usa o EQUALS...
+
+            Clients a = new Clients { Name = "Maria", Email = "maria@gmail.com"};
+            Clients b = new Clients { Name = "Alex", Email = "alex@gmail.com"};
 
             Console.WriteLine(a.Equals(b));
-
-            a = "Maria";
-            b = "Maria";
-
-            Console.WriteLine(a.Equals(b));
-
-
-            a = "Maria";
-            b = "Alex";
-
             Console.WriteLine(a.GetHashCode());
             Console.WriteLine(b.GetHashCode());
 
-            a = "Maria";
-            b = "Maria";
+            /*  RESULTADO
+                    False
+                    298195988
+                    465141759
+             */
 
-            Console.WriteLine(a.GetHashCode());
-            Console.WriteLine(b.GetHashCode());
+            Clients c = new Clients { Name = "Maria", Email = "maria@gmail.com" };
+            Clients d = new Clients { Name = "Maria Lord", Email = "maria@gmail.com" };
 
-            /* Resultado
-            
-                True
-                -66323339
-                475641941
-                -66323339
-                -66323339
+            Console.WriteLine(c.Equals(d));
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+
+            /*  RESULTADO
+                    True
+                    298195988
+                    298195988
+             */
+
+            //o IGUAL IGUAL ele tá comparando a referência do ponteiro de memória do objeto
+            Console.WriteLine(a == b);
+
+            //são iguais do ponto de vista do conteúdo, mas difentes do ponto de vista da referência
+            Console.WriteLine(c.Equals(d));
+            Console.WriteLine(c == d);
+
+            /*  RESULTADO
+                    False
+                    True
+                    False
             */
-
 
         }
     }
