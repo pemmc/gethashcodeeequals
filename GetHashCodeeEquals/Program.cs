33733c1e99ba39be1794eeb21adfd960ec6066e2
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-22 16:07:53 -0300

    GetHashCod e Eequals

---
+++ GetHashCodeeEquals/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GetHashCodeeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Alex";
+
+            Console.WriteLine(a.Equals(b));
+
+            a = "Maria";
+            b = "Maria";
+
+            Console.WriteLine(a.Equals(b));
+
+
+            a = "Maria";
+            b = "Alex";
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            a = "Maria";
+            b = "Maria";
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            /* Resultado
+            
+                True
+                -66323339
+                475641941
+                -66323339
+                -66323339
+            */
+
+
+        }
+    }
+}
